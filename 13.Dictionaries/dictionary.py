cdb44b4810a759280c1f6d0d33b3d4097a9a44f9
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 23:11:10 +0200

    added dictionary

---
+++ 13.Dictionaries/dictionary.py
@@ -0,0 +1,20 @@
+my_dictionary = {
+    'name': 'John',
+    'nationality': 'African',
+    'Qualification': 'College'
+}
+# mixing of data types
+my_dictionary2 = {
+    'name': 'John',
+    'nationality': 'African',
+    'age': 87,
+    'is_tall': False,
+    'Qualification': 'College',
+    'friends': ['tim', 'Divin', 'Ernest']
+
+}
+x = my_dictionary['name']
+
+print(my_dictionary)
+print(my_dictionary['name'])
+print(my_dictionary2['friends'])
