b6a11f6920ba8c3121c56eeefda2a65fb6ae0e99
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 23:32:14 +0200

    added for Loop

---
+++ 15.For Loops/forLoop.py
@@ -0,0 +1,13 @@
+myList = ['ji', 'jo', 'ju']
+
+
+for letter in 'Hello':
+    print(letter)
+
+for element in myList:
+    print(element)
+    if element =='jo':
+        break
+for x in range(10):
+    print(x)
+
