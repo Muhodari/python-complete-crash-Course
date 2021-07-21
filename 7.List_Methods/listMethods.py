854ef535ce8ccdff2b09340619d9781ea7685449
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 19:07:44 +0200

    append element to list

--- 7.List_Methods/listMethods.py
+++ 7.List_Methods/listMethods.py
@@ -1,5 +1,10 @@
 list1 = [1, 2, 3, 4, 5]
 list2 = ['banana', 'Apples', 'mango', 'oranges']
 
+# combination of two list
 list1.extend(list2)
 print(list1)
+
+# add element in list
+list2.append('pineApple')
+print(list2)
