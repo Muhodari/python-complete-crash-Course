c5a5a63042e0e51b2fa4c72a24c2d8a1cf1a66e1
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 06:03:23 +0200

    added numbers

---
+++ 3.Numbers/numbers.py
@@ -0,0 +1,28 @@
+from math import *
+
+number = 79
+print(78)
+print(number)
+
+# addition
+print(78 + 79.956)
+# subtraction
+print(67 - 89.4)  # the same approach to division and multiplication
+# convert a number to a string
+number2 = str(number)
+print("converted tp string:" + number2)
+
+# print the number regardless of the sign with abs()
+number = -6
+print(abs(number))
+
+# get max number and min
+print(max(3, 56, 7, 20, 78, 0, 56))
+print(min(3, 56, 7, 20, 78, 0, 56))
+# ROUNDING and convert to binary string
+print(round(45.765))
+print(bin(345))
+
+# square root
+print(sqrt(100))
+
