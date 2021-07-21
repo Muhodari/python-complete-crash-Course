195a7a517e8d3a547daf838468387f650d852491
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 19:34:43 +0200

    finished with list methods

--- 7.List_Methods/listMethods.py
+++ 7.List_Methods/listMethods.py
@@ -1,22 +1,22 @@
-list1 = [1, 2, 3, 4, 5]
+list1 = [4, 3, 5, 1, 2]
 list2 = ['banana', 'Apples', 'mango', 'oranges']
+list3 = list2.copy()
 
+# arrange numbers in ascending order
+list1.sort()
 # combination of two list
 list1.extend(list2)
-print(list1)
 # add element in list
 list2.append('pineApple')
-print(list2)
 # put element at a specific index
 list2.insert(1, 'pineApple')
-print(list2)
 # remove element in the list
 list2.remove('banana')
-print(list2)
+list2.pop(2)
+del list2[0]
 # count number of occurrence
 print(list2.count('pineApple'))
-
-
+# remove last element from the list
+list2.pop()
 # delete the whole list
 list2.clear()
-print(list2)
