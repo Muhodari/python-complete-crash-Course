91be94d0c4bac2ed538940faf529dcb0d38fdb41
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 06:38:21 +0200

    change the element in the list

--- 6.List/list.py
+++ 6.List/list.py
@@ -1,7 +1,15 @@
 countries = ['Rwanda', 'Ghana', 'Nigeria', 'Australia']
+
 print(countries)
 print(countries[0])
 print(countries[0][3])
 # print from a given index up to the end  and range
 print(countries[1:])
 print(countries[1:3])
+
+# get variable type
+print(type(countries))
+
+# update element in list
+countries[0] = 'united Kingdom'
+print(countries)
