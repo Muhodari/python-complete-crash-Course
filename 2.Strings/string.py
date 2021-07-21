b4fbfdaac0629f48da9981b48e9a298f8817c9c2
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 05:41:00 +0200

    added string

--- 2.Strings/string.py
+++ 2.Strings/string.py
@@ -1,3 +1,24 @@
+name = "Tim"
+
 print("Hi. how are you?")
 # new line
-print("Hi.\n How are you?");
+print("Hi.\n How are you?")
+
+# get the first latter of string
+print(name[0])
+# to upper case
+print(name.upper())
+# lowe case
+print(name.lower())
+# check whether name is lower
+print(name.islower())
+# check whether name is upper
+print(name.isupper())
+# convert to lowe and check
+print(name.lower().islower())
+# length of string
+print(len(name))
+# get index of element
+print(name.index('i'))
+# replace element in text
+print(name.replace('m', 'sang'))
