383757e45859d9ee3bb8a89fa52df7b74f9549e5
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 19:18:12 +0200

    added function to deal with string

--- 7.List_Methods/listMethods.py
+++ 7.List_Methods/listMethods.py
@@ -4,7 +4,19 @@ list2 = ['banana', 'Apples', 'mango', 'oranges']
 # combination of two list
 list1.extend(list2)
 print(list1)
-
 # add element in list
 list2.append('pineApple')
+print(list2)
+# put element at a specific index
+list2.insert(1, 'pineApple')
+print(list2)
+# remove element in the list
+list2.remove('banana')
+print(list2)
+# count number of occurrence
+print(list2.count('pineApple'))
+
+
+# delete the whole list
+list2.clear()
 print(list2)
