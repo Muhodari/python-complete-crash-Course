c4e154439108e0b8010ef48e90c5b933dde1a41a
Muhodari Sage <sagemuho@gmail.com>
2021-07-21 06:33:23 +0200

    access element from list

---
+++ 6.List/list.py
@@ -0,0 +1,7 @@
+countries = ['Rwanda', 'Ghana', 'Nigeria', 'Australia']
+print(countries)
+print(countries[0])
+print(countries[0][3])
+# print from a given index up to the end  and range
+print(countries[1:])
+print(countries[1:3])
